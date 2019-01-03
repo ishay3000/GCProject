913b2846db6548d276dff2f941705e439ae4541d
ishay muchtar <ishay3000@gmail.com>
2019-01-03 09:17:22 +0200

    Added base functionality of mvvm. Added MenuCard template.

---
+++ GCProject/Models/MenuCard.cs
@@ -0,0 +1,34 @@
+﻿namespace GCProject.Models
+{
+	/// <summary>
+	/// A class that represents a card in the menu window
+	/// </summary>
+	public class MenuCard
+	{
+		private string _cardText;
+		private string _imagePath;
+
+		/// <summary>
+		/// Constructs a card with its text and image source
+		/// </summary>
+		/// <param name="cardText"></param>
+		/// <param name="imagePath"></param>
+		public MenuCard(string cardText, string imagePath)
+		{
+			_cardText = cardText;
+			_imagePath = imagePath;
+		}
+
+		public string CardText
+		{
+			get => _cardText;
+			set => _cardText = value;
+		}
+
+		public string ImagePath
+		{
+			get => _imagePath;
+			set => _imagePath = value;
+		}
+	}
+}
---
+++ GCProject/ViewModels/MenuCardViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCProject.Models;
+
+namespace GCProject.ViewModels
+{
+	class MenuCardViewModel : AbsViewModel
+	{
+		private string _cardText;
+		private string _imagePath;
+		private readonly ObservableCollection<MenuCard> _cards = new ObservableCollection<MenuCard>();
+		
+		public string CardText
+		{
+			get => _cardText;
+			set
+			{
+				if (_cardText != value)
+				{
+					_cardText = value;
+					OnPropertyChanged("CardText");
+				}
+			}
+		}
+
+		public string ImagePath
+		{
+			get => _imagePath;
+			set
+			{
+				if (_imagePath != value)
+				{
+					_imagePath = value;
+					OnPropertyChanged("ImagePath");
+				}
+			}
+		}
+
+		public ObservableCollection<MenuCard> Cards => _cards;
+	}
+}
