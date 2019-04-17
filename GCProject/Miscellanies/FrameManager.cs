a64d01641116f12dd38908bc98d81007f7ae82f1
ishay <ishay3000@gmail.com>
2019-04-17 18:10:56 +0300

    Remapped the scan button to show the form instead of the results

--- GCProject/Miscellanies/FrameManager.cs
+++ GCProject/Miscellanies/FrameManager.cs
@@ -11,64 +11,55 @@ using GCProject.Views;
 namespace GCProject.miscellanies
 {
 
-	public enum ControlsTitles
-	{
-		Cards,
-		Settings,
-		ScanResults
-	}
+    public enum ControlsTitles
+    {
+        Cards,
+        Settings,
+        ScanForm,
+        ScanResults
+    }
     static class FrameManager
     {
-	    public static Frame MainFrame { get; set; }
-		// TODO either find a use for this or remove it you lazy baboon
-	    private static readonly Dictionary<ControlsTitles, 
-		    Tuple<UserControl, string>> ControlsTitlesDict;
+        public static Frame MainFrame { get; set; }
+        // TODO either find a use for this or remove it you lazy baboon
+        private static readonly Dictionary<ControlsTitles,
+            Tuple<UserControl, string>> ControlsTitlesDict;
 
-	    static FrameManager()
-	    {
-		    ControlsTitlesDict = new Dictionary<ControlsTitles, Tuple<UserControl, string>>();
-			InitDictionary();
-	    }
+        static FrameManager()
+        {
+            ControlsTitlesDict = new Dictionary<ControlsTitles, Tuple<UserControl, string>>();
+            InitDictionary();
+        }
 
-	    private static void InitDictionary()
-	    {
-		    ControlsTitlesDict[ControlsTitles.Cards] = 
-			    new Tuple<UserControl, string>(new CustomCardControl(), "Welcome to Telephony Scanner");
-		    ControlsTitlesDict[ControlsTitles.ScanResults] =
-			    new Tuple<UserControl, string>(new ScanResultsControl(), "Scan Results");
-		}
+        /// <summary>
+        /// initiates the user controls dict
+        /// </summary>
+        private static void InitDictionary()
+        {
+            ControlsTitlesDict[ControlsTitles.Cards] =
+                new Tuple<UserControl, string>(new CustomCardControl(), "Welcome to Telephony Scanner");
 
-		/// <summary>
-		/// Moves the frame to a different page
-		/// </summary>
-		/// <param name="pageTitle">the page title</param>
-		public static void MovePage(ControlsTitles pageTitle)
-	    {
-		    if (ControlsTitlesDict.ContainsKey(pageTitle))
-		    {
-			    var controlTuple = ControlsTitlesDict[pageTitle];
-				// change page
-			    MainFrame.Content = controlTuple.Item1;
-				// change title
-			    WindowViewModel.INSTANCE.WindowTitle = controlTuple.Item2;
-		    }
-	    }
+            ControlsTitlesDict[ControlsTitles.ScanResults] =
+                new Tuple<UserControl, string>(new ScanResultsControl(), "Scan Results");
 
-	    //public static void PutExtra(string key, UserControl value)
-	    //{
-	    // ExtrasDictionary[key] = value;
-	    //}
+            ControlsTitlesDict[ControlsTitles.ScanForm] =
+                new Tuple<UserControl, string>(new ScanFormControl(), "Scan Results");
+        }
 
-	    //public static object GetExtra(string key)
-	    //{
-	    // if (ExtrasDictionary.ContainsKey(key))
-	    // {
-	    //  return ExtrasDictionary[key];
-	    // }
-	    // else
-	    // {
-	    //  return null;
-	    // }
-	    //}
+        /// <summary>
+        /// Moves the frame to a different page
+        /// </summary>
+        /// <param name="pageTitle">the page title</param>
+        public static void MovePage(ControlsTitles pageTitle)
+        {
+            if (ControlsTitlesDict.ContainsKey(pageTitle))
+            {
+                var controlTuple = ControlsTitlesDict[pageTitle];
+                // change page
+                MainFrame.Content = controlTuple.Item1;
+                // change title
+                WindowViewModel.INSTANCE.WindowTitle = controlTuple.Item2;
+            }
+        }
     }
 }
